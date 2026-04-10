9dd2c7d3d166a5f2ed2fc31358494b74ab2b0a66
bnatann698-sketch <bnatann698@gmail.com>
2026-04-10 16:29:58 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://uqjedmlcebtriwocunsv.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxamVkbWxjZWJ0cml3b2N1bnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3OTI2MDYsImV4cCI6MjA5MTM2ODYwNn0.xWe_uQJ-PkjQ3ZnzK7PPh1upZGizMkbIaSt-pq7CUM0";
-
-const banco = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
